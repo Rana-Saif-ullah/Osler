0633761170a9c6d4a37a609abb0b3632c7e62cc0
Rana Saifullah <111068945+Rana-Saif-ullah@users.noreply.github.com>
2023-11-03 18:35:50 +0500

    added

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@
 //     el: document.querySelector('#smooth__body'),
 //     smooth: true
 // });
-var menuIcon = document.querySelector('menu__icon');
+var menuIcon = document.querySelector('.menu__icon'); // Add a period before class name
+var dropSection = document.querySelector('.drop__section'); // Add a period before class name
 
-menuIcon.addEventListener('click', function () {
-    menuIcon.classList.toggle('active');
+menuIcon.addEventListener("click", () => {
+    dropSection.classList.add('active__drop__section');
 });
+
+// var menuCloseIcon = document.querySelector('.menu__icon'); // Add a period before class name
+// var dropSection = document.querySelector('.drop__section'); // Add a period before class name
+
+// menuIcon.addEventListener("click", () => {
+//     dropSection.classList.remove('active__drop__section');
+// });
